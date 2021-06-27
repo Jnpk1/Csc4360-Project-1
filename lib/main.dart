4b56916052bd8499dde0597246c6c82ff82bfb6b
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-06-27 15:22:37 -0400

    moved home widget into home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:memoclub/screens/home.dart';
 import 'package:memoclub/shared/loading.dart';
 
 void main() {
   // This needs to be called before any Firebase services can be used
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(App());
+  runApp(AppToInitializeFirebase());
 }
 
 /// We are using a StatefulWidget such that we only create the [Future] once,
 /// no matter how many times our widget rebuild.
-/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// If we used a [StatelessWidget], in the event where [AppToInitializeFirebase] is rebuilt, that
 /// would re-initialize FlutterFire and make our application re-enter loading state,
 /// which is undesired.
-class App extends StatefulWidget {
+class AppToInitializeFirebase extends StatefulWidget {
   // Create the initialization Future outside of `build`:
   @override
-  _AppState createState() => _AppState();
+  _AppToInitializeFirebaseState createState() =>
+      _AppToInitializeFirebaseState();
 }
 
-class _AppState extends State<App> {
+class _AppToInitializeFirebaseState extends State<AppToInitializeFirebase> {
   /// The future is part of the state of our widget. We should not call `initializeApp`
   /// directly inside [build].
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
@@ -52,59 +54,14 @@ class _AppState extends State<App> {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
