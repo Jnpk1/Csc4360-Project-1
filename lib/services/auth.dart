20c835c5f7c924c3c3bb26d1cf4ae011e3cbb3ac
James <55366454+Jnpk1@users.noreply.github.com>
2021-07-01 21:51:05 -0400

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:memoclub/models/Member.dart';
 
@@ -10,6 +10,7 @@ import 'database.dart';
 class AuthService with ChangeNotifier {
   Member currentMember = Member();
   FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
   User? user;
   AuthService();
 
@@ -73,6 +74,23 @@ class AuthService with ChangeNotifier {
       print(e);
       return null;
     }
+  }
+  Future<UserCredential?> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   // // Haven't tested yet
