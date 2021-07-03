d1e0caf4bc010ea80a269b77243879fc9d976a85
Drew Hartman <drewdavishartman@gmail.com>
2021-07-03 14:54:16 -0400

    initial commit to settings adding buttons

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:memoclub/screens/home.dart';
+import 'package:memoclub/screens/register.dart';
+import 'package:memoclub/screens/sign_in.dart';
+import 'package:memoclub/screens/styles/buttons.dart';
 import 'package:memoclub/screens/styles/colors.dart';
+import 'package:memoclub/screens/welcome.dart';
 import 'package:memoclub/shared/appbar.dart';
 import 'package:memoclub/shared/drawer.dart';
 
@@ -12,15 +16,73 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  AuthService _auth = Provider.of<AuthService>(context, listen: false);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: memoAppBar(context, "Settings"),
       backgroundColor: kBackgroundColor,
+      appBar: memoAppBar(context, "Settings"),
       body: Center(
-        child: roomButtons(context),
+        child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 15.0),
+                MaterialButton(
+                    elevation: buttonThemeElevation,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(buttonBorderRadius),
+                    ),
+                    child: Text('Change Password',
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            ?.copyWith(color: kOnButtonColor)),
+                    color: kButtonColor,
+                    onPressed: () async {
+                      Navigator.pushNamed(context, Register.routeName);
+                    }),
+                SizedBox(height: 15.0),
+                MaterialButton(
+                    elevation: buttonThemeElevation,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(buttonBorderRadius),
+                    ),
+                    child: Text('Change Username',
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            ?.copyWith(color: kOnButtonColor)),
+                    color: kButtonColor,
+                    onPressed: () async {
+                      Navigator.pushNamed(context, SignIn.routeName);
+                    }),
+                SizedBox(height: 15.0),
+                MaterialButton(
+                    onPressed: () async {
+                      bool didSignOut = await _auth.signOut();
+                      if (didSignOut) {
+                        // this navigator command pops all history from the navigator,
+                        // and then sends them to home screen. This prevent them from
+                        // signing out, then pressing back on native button to re-enter the app
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                            Welcome.routeName, (Route<dynamic> route) => false);
+                      }
+                    },
+                    elevation: buttonThemeElevation,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Text('Sign Out',
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            ?.copyWith(color: kOnButtonColor)),
+                    color: kButtonColor),
+              ],
+            )),
       ),
-      drawer: memoDrawer(context),
     );
   }
 }
