59c59225d25ca18a695f46c416cb7f60a4e8eb7b
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-05 18:49:21 -0400

    finished profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -19,47 +19,93 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     Member currentMember = Provider.of<Member>(context);
+    String _facebookLink = currentMember.connectedSocials?["facebook"] ?? '';
+    String _instagramLink = currentMember.connectedSocials?["instagram"] ?? '';
     return Scaffold(
       backgroundColor: kBackgroundColor,
       appBar: memoAppBar(context, "Profile"),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 10.0),
+          child: ListView(children: <Widget>[
+            Card(
+                child: ListTile(
+              leading: Icon(
+                Icons.facebook,
+                size: 56.0,
+                color: Colors.white,
               ),
-              Text(
-                "Connected Social Acocunts",
+              tileColor: kPrimaryColor,
+              title: Text(
+                'Facebook Account',
                 style: Theme.of(context)
                     .textTheme
-                    .headline5
-                    ?.copyWith(color: kOnBackgroundColor),
+                    .bodyText1
+                    ?.copyWith(color: Colors.white),
               ),
-              SizedBox(
-                height: 40,
+              onTap: () async {
+                if (_facebookLink.isNotEmpty) {
+                  await canLaunch(_facebookLink)
+                      ? await launch(_facebookLink)
+                      : throw 'Could not launch $_facebookLink';
+                }
+              },
+              subtitle: _facebookLink.isEmpty
+                  ? Text(
+                      "No Facebook link available.",
+                      style: Theme.of(context)
+                          .textTheme
+                          .caption
+                          ?.copyWith(color: Colors.white),
+                    )
+                  : (Text(
+                      "$_facebookLink",
+                      style: Theme.of(context)
+                          .textTheme
+                          .caption
+                          ?.copyWith(color: Colors.white),
+                    )),
+            )),
+            Card(
+                child: ListTile(
+              leading: Icon(
+                Icons.camera_alt_outlined,
+                size: 56.0,
+                color: Colors.white,
               ),
-              Icon(Icons.facebook),
-              facebookLink(
-                  context, currentMember.connectedSocials?['facebook'] ?? ''),
-              SizedBox(
-                height: 40,
+              tileColor: kPrimaryColor,
+              title: Text(
+                'Instagram Account',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    ?.copyWith(color: Colors.white),
               ),
-              Icon(Icons.camera_alt),
-              instagramLink(
-                  context, currentMember.connectedSocials?['instagram'] ?? ''),
-              SizedBox(
-                height: 40,
-              ),
-              Expanded(
-                child: Container(),
-              ),
-              roomButtons(context),
-              SizedBox(
-                height: 100,
-              ),
-            ],
-          ),
+              onTap: () async {
+                if (_instagramLink.isNotEmpty) {
+                  await canLaunch(_instagramLink)
+                      ? await launch(_instagramLink)
+                      : throw 'Could not launch $_instagramLink';
+                }
+              },
+              subtitle: _instagramLink.isEmpty
+                  ? Text(
+                      "No Instagram link available.",
+                      style: Theme.of(context)
+                          .textTheme
+                          .caption
+                          ?.copyWith(color: Colors.white),
+                    )
+                  : (Text(
+                      "$_instagramLink",
+                      style: Theme.of(context)
+                          .textTheme
+                          .caption
+                          ?.copyWith(color: Colors.white),
+                    )),
+            )),
+            roomButtons(context),
+          ]),
         ),
       ),
       drawer: memoDrawer(context),
@@ -67,60 +113,22 @@ class _ProfileState extends State<Profile> {
   }
 }
 
-Widget facebookLink(context, String possibleUrl) {
-  return possibleUrl.isEmpty
-      ? Container(child: Text("Facebook is not connected."))
-      : StyledButton(
-          text: possibleUrl,
-          onPressed: () async => await canLaunch(possibleUrl)
-              ? await launch(possibleUrl)
-              : throw 'Could not launch $possibleUrl');
-}
-
-Widget instagramLink(context, String possibleUrl) {
-  return possibleUrl.isEmpty
-      ? Container(child: Text("Instagram is not connected."))
-      : StyledButton(
-          text: possibleUrl,
-          onPressed: () async => await canLaunch(possibleUrl)
-              ? await launch(possibleUrl)
-              : throw 'Could not launch $possibleUrl');
-}
-// https://firebase.flutter.dev/docs/firestore/usage#realtime-changes
-// use ^^ to track connected social media accounts.
-
-// class UserInformation extends StatefulWidget {
-//   @override
-//   _UserInformationState createState() => _UserInformationState();
+// Widget facebookLink(context, String possibleUrl) {
+//   return possibleUrl.isEmpty
+//       ? Container(child: Text("Facebook is not connected."))
+//       : StyledButton(
+//           text: possibleUrl,
+//           onPressed: () async => await canLaunch(possibleUrl)
+//               ? await launch(possibleUrl)
+//               : throw 'Could not launch $possibleUrl');
 // }
 
-// class _UserInformationState extends State<UserInformation> {
-//   final Stream<QuerySnapshot> _usersStream =
-//       FirebaseFirestore.instance.collection('users').snapshots();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<QuerySnapshot>(
-//       stream: _usersStream,
-//       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//         if (snapshot.hasError) {
-//           return Text('Something went wrong');
-//         }
-
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return Text("Loading");
-//         }
-
-//         return new ListView(
-//           children: snapshot.data.docs.map((DocumentSnapshot document) {
-//             Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-//             return new ListTile(
-//               title: new Text(data['full_name']),
-//               subtitle: new Text(data['company']),
-//             );
-//           }).toList(),
-//         );
-//       },
-//     );
-//   }
+// Widget instagramLink(context, String possibleUrl) {
+//   return possibleUrl.isEmpty
+//       ? Container(child: Text("Instagram is not connected."))
+//       : StyledButton(
+//           text: possibleUrl,
+//           onPressed: () async => await canLaunch(possibleUrl)
+//               ? await launch(possibleUrl)
+//               : throw 'Could not launch $possibleUrl');
 // }
