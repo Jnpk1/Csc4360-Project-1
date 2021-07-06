4171c49226b59bc754a86b470f32cad5a4822e81
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-05 20:51:08 -0400

    added social page username

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -21,92 +21,129 @@ class _ProfileState extends State<Profile> {
     Member currentMember = Provider.of<Member>(context);
     String _facebookLink = currentMember.connectedSocials?["facebook"] ?? '';
     String _instagramLink = currentMember.connectedSocials?["instagram"] ?? '';
+    String _currentUsername = currentMember.username;
     return Scaffold(
       backgroundColor: kBackgroundColor,
       appBar: memoAppBar(context, "Profile"),
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 10.0),
-          child: ListView(children: <Widget>[
-            Card(
-                child: ListTile(
-              leading: Icon(
-                Icons.facebook,
-                size: 56.0,
-                color: Colors.white,
-              ),
-              tileColor: kPrimaryColor,
-              title: Text(
-                'Facebook Account',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText1
-                    ?.copyWith(color: Colors.white),
-              ),
-              onTap: () async {
-                if (_facebookLink.isNotEmpty) {
-                  await canLaunch(_facebookLink)
-                      ? await launch(_facebookLink)
-                      : throw 'Could not launch $_facebookLink';
-                }
-              },
-              subtitle: _facebookLink.isEmpty
-                  ? Text(
-                      "No Facebook link available.",
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption
-                          ?.copyWith(color: Colors.white),
-                    )
-                  : (Text(
-                      "$_facebookLink",
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption
-                          ?.copyWith(color: Colors.white),
-                    )),
-            )),
-            Card(
-                child: ListTile(
-              leading: Icon(
-                Icons.camera_alt_outlined,
-                size: 56.0,
-                color: Colors.white,
-              ),
-              tileColor: kPrimaryColor,
-              title: Text(
-                'Instagram Account',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText1
-                    ?.copyWith(color: Colors.white),
-              ),
-              onTap: () async {
-                if (_instagramLink.isNotEmpty) {
-                  await canLaunch(_instagramLink)
-                      ? await launch(_instagramLink)
-                      : throw 'Could not launch $_instagramLink';
-                }
-              },
-              subtitle: _instagramLink.isEmpty
-                  ? Text(
-                      "No Instagram link available.",
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption
-                          ?.copyWith(color: Colors.white),
-                    )
-                  : (Text(
-                      "$_instagramLink",
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption
-                          ?.copyWith(color: Colors.white),
-                    )),
-            )),
-            roomButtons(context),
-          ]),
-        ),
+        minimum: EdgeInsets.symmetric(vertical: 40.0, horizontal: 10.0),
+        child: Column(children: <Widget>[
+          Card(
+              margin: EdgeInsets.all(10.0),
+              elevation: 0,
+              child: ListTile(
+                leading: Icon(
+                  Icons.face,
+                  size: 56.0,
+                  color: kPrimaryColor,
+                ),
+                tileColor: kBackgroundColor,
+                title: Text(
+                  "username: $_currentUsername",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1
+                      ?.copyWith(color: kPrimaryColor),
+                ),
+              )),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            "Connected Socials",
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                ?.copyWith(color: kPrimaryColor),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Card(
+              margin: EdgeInsets.all(10.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.facebook,
+                  size: 56.0,
+                  color: Colors.white,
+                ),
+                tileColor: kPrimaryColor,
+                title: Text(
+                  'Facebook Account',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1
+                      ?.copyWith(color: Colors.white),
+                ),
+                onTap: () async {
+                  if (_facebookLink.isNotEmpty) {
+                    await canLaunch(_facebookLink)
+                        ? await launch(_facebookLink)
+                        : throw 'Could not launch $_facebookLink';
+                  }
+                },
+                subtitle: _facebookLink.isEmpty
+                    ? Text(
+                        "No Facebook link available.",
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption
+                            ?.copyWith(color: Colors.white),
+                      )
+                    : (Text(
+                        "$_facebookLink",
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption
+                            ?.copyWith(color: Colors.white),
+                      )),
+              )),
+          Card(
+              margin: EdgeInsets.all(10.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.camera_alt_outlined,
+                  size: 56.0,
+                  color: Colors.white,
+                ),
+                tileColor: kPrimaryColor,
+                title: Text(
+                  'Instagram Account',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1
+                      ?.copyWith(color: Colors.white),
+                ),
+                onTap: () async {
+                  if (_instagramLink.isNotEmpty) {
+                    await canLaunch(_instagramLink)
+                        ? await launch(_instagramLink)
+                        : throw 'Could not launch $_instagramLink';
+                  }
+                },
+                subtitle: _instagramLink.isEmpty
+                    ? Text(
+                        "No Instagram link available.",
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption
+                            ?.copyWith(color: Colors.white),
+                      )
+                    : (Text(
+                        "$_instagramLink",
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption
+                            ?.copyWith(color: Colors.white),
+                      )),
+              )),
+          Expanded(child: Container()),
+          roomButtons(context),
+          SizedBox(
+            height: 20,
+          ),
+        ]),
+        // ),
       ),
       drawer: memoDrawer(context),
     );
