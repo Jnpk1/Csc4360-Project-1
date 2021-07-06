4e8238afc98b9639c138f857507077002e70bad6
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-05 21:24:44 -0400

    cosmetic changes, added logout to drawer

--- lib/shared/drawer.dart
+++ lib/shared/drawer.dart
@@ -3,13 +3,14 @@ import 'package:memoclub/screens/home.dart';
 import 'package:memoclub/screens/profile.dart';
 import 'package:memoclub/screens/settings.dart';
 import 'package:memoclub/screens/styles/colors.dart';
+import 'package:memoclub/screens/welcome.dart';
 
 Widget memoDrawer(BuildContext context) {
   return Drawer(
       // Add a ListView to the drawer. This ensures the user can scroll
       // through the options in the drawer if there isn't enough vertical
       // space to fit everything.
-      
+
       child: ListView(
 
           // Important: Remove any padding from the ListView.
@@ -27,7 +28,10 @@ Widget memoDrawer(BuildContext context) {
         ),
         ListTile(
           title: Text('Settings'),
-          leading: Icon(Icons.settings),
+          leading: Icon(
+            Icons.settings,
+            color: kPrimaryColor,
+          ),
           onTap: () {
             // Update the state of the app
             // ...
@@ -38,7 +42,10 @@ Widget memoDrawer(BuildContext context) {
         ),
         ListTile(
             title: Text('Profile'),
-            leading: Icon(Icons.face),
+            leading: Icon(
+              Icons.face,
+              color: kPrimaryColor,
+            ),
             onTap: () {
               // Update the state of the app
               // ...
@@ -48,13 +55,31 @@ Widget memoDrawer(BuildContext context) {
             }),
         ListTile(
             title: Text('Home'),
-            leading: Icon(Icons.home),
+            leading: Icon(
+              Icons.home,
+              color: kPrimaryColor,
+            ),
             onTap: () {
               // Update the state of the app
               // ...
               // Then close the drawer
               Navigator.pop(context);
               Navigator.pushNamed(context, Home.routeName);
+            }),
+        ListTile(
+            title: Text('Logout'),
+            leading: Icon(
+              Icons.logout,
+              color: kPrimaryColor,
+            ),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              // Navigator.pop(context);
+              // Navigator.pushNamed(context, Home.routeName);
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                  Welcome.routeName, (Route<dynamic> route) => false);
             })
       ]));
 }
